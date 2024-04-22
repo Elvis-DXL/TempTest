f5bff3169f53d6adeb0c7a8b45e3a1cec67a4e84
daixiaolong <982424518@qq.com>
2024-04-22 15:24:40 +0800

    添加测试

--- src/main/java/project/base/DSUtil.java
+++ src/main/java/project/base/DSUtil.java
@@ -1,18 +1,26 @@
 package project.base;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
 
 /**
  * @Author : 慕君Dxl
  * @CreateTime : 2024/4/19 16:45
  */
-public class DSUtil {
+public final class DSUtil {
+    //私有化构造器，禁止创建对象
     private DSUtil() {
         throw new AssertionError("Utility classes do not allow instantiation");
     }
 
+    /****************************************以上为私有构造器，以下为常量****************************************/
+    //地球半径,单位:m
+    public static final double EARTH_RADIUS = 6371393;
+
+    //时间格式化枚举
     public enum DatePattern {
         yyyy_MM_dd_HH_mm_ss_SSS("yyyy-MM-dd HH:mm:ss.SSS"),
         yyyy_MM_dd_HH_mm_ss("yyyy-MM-dd HH:mm:ss"),
@@ -42,6 +50,8 @@ public class DSUtil {
         c_HH_mm_ss("HH时mm分ss秒"),
         c_HH_mm("HH时mm分"),
         c_yyyy_MM_dd_EE("yyyy年MM月dd日 EE"),
+        x_yyyy_MM_dd_EE("yyyy/MM/dd EE"),
+        yyyy_MM_dd_EE("yyyy-MM-dd EE"),
         c_EE("EE"),
         x_yyyy_MM_dd_HH_mm_ss_SSS("yyyy/MM/dd HH:mm:ss.SSS"),
         x_yyyy_MM_dd_HH_mm_ss("yyyy/MM/dd HH:mm:ss"),
@@ -52,13 +62,6 @@ public class DSUtil {
         xh_yyyy_MM_dd_HH("yyyy_MM_dd_HH"),
         xh_yyyy_MM_dd("yyyy_MM_dd"),
         xh_yyyy_MM("yyyy_MM"),
-        yyyy("yyyy"),
-        MM("MM"),
-        dd("dd"),
-        HH("HH"),
-        mm("mm"),
-        ss("ss"),
-        SSS("SSS"),
         ;
         private final String val;
 
@@ -71,6 +74,7 @@ public class DSUtil {
         }
     }
 
+    /*****************************************以上为常量，以下为工具函数*****************************************/
     public static LocalDateTime dayStart(LocalDateTime time) {
         return time.withHour(0).withMinute(0).withSecond(0).withNano(0);
     }
@@ -95,11 +99,33 @@ public class DSUtil {
         return dayEnd(time.with(TemporalAdjusters.lastDayOfYear()));
     }
 
+    public static String formatTime(LocalDateTime time, String datePattern) {
+        return DateTimeFormatter.ofPattern(datePattern).format(time);
+    }
+
     public static String formatTime(LocalDateTime time, DatePattern datePattern) {
         return DateTimeFormatter.ofPattern(datePattern.val()).format(time);
     }
 
+    public static LocalDateTime parseTime(String timeStr, String datePattern) {
+        return LocalDateTime.parse(timeStr, DateTimeFormatter.ofPattern(datePattern));
+    }
+
     public static LocalDateTime parseTime(String timeStr, DatePattern datePattern) {
         return LocalDateTime.parse(timeStr, DateTimeFormatter.ofPattern(datePattern.val()));
+    }
+
+    public static LocalDateTime dateToLocal(Date date) {
+        return null != date ? date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime() : null;
+    }
+
+    public static Date localToDate(LocalDateTime localDateTime) {
+        return null != localDateTime ? Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()) : null;
+    }
+
+    public static double lngLatMeter(double srcLng, double srcLat, double aimLng, double aimLat) {
+        return EARTH_RADIUS * Math.acos(Math.cos(Math.toRadians(srcLat)) * Math.cos(Math.toRadians(aimLat))
+                * Math.cos(Math.toRadians(srcLng) - Math.toRadians(aimLng))
+                + Math.sin(Math.toRadians(srcLat)) * Math.sin(Math.toRadians(aimLat)));
     }
 }
