bfbddfd65f3ab93fa15a2c2eb2acbde631803a59
daixiaolong <982424518@qq.com>
2024-04-23 08:43:46 +0800

    添加测试

--- src/main/java/com/elvis/test/ElvisTest.java
+++ src/main/java/com/elvis/test/ElvisTest.java
@@ -1,12 +1,14 @@
 package com.elvis.test;
 
 
+import project.base.DSUtil;
+
 /**
  * @author : Elvis
  * @since : 2022/4/28 11:35
  */
 public class ElvisTest {
     public static void main(String[] args) {
-        
+        System.out.println(DSUtil.Symbol.DH.srcNotContainAim("asdfas,asdfas,asdfsf,sdf","fsfsd"));
     }
 }
