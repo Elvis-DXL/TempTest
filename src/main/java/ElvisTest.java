1b857af34eda27d76964f2d313dbbea8dd003dc9
daixiaolong <982424518@qq.com>
2024-04-28 17:57:50 +0800

    添加测试

--- src/main/java/ElvisTest.java
+++ src/main/java/ElvisTest.java
@@ -1,9 +1,11 @@
+import project.base.DSUtil;
+
 /**
  * @author : Elvis
  * @since : 2022/4/28 11:35
  */
 public class ElvisTest {
     public static void main(String[] args) {
-
+System.out.println(DSUtil.randomNumberStrByLength(6));
     }
 }
--- src/main/java/com/elvis/test/newproject/utils/DTool.java
+++
@@ -1,8 +0,0 @@
-package com.elvis.test.newproject.utils;
-
-/**
- * @Author : 慕君Dxl
- * @CreateTime : 2023/8/28 15:33
- */
-public class DTool {
-}
