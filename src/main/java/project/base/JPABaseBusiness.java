59d6d7c3c35be9e35101a7c9484a67dc720c24d6
daixiaolong <982424518@qq.com>
2024-05-14 17:35:41 +0800

    添加测试

--- src/main/java/project/base/JPABaseBusiness.java
+++ src/main/java/project/base/JPABaseBusiness.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
@@ -21,8 +25,8 @@ import static project.base.DSUtil.PageResp;
  * @Author : 慕君Dxl
  * @CreateTime : 2024/4/25 11:31
  */
-public abstract class JPABaseBusiness<ID extends Serializable, ENTITY, ENTITY_VO, ADD_CMD, MODIFY_CMD,
-        QUERY_CMD extends PageReq, DAO extends JpaRepository<ENTITY, ID> & JpaSpecificationExecutor<ENTITY>> {
+public abstract class JPABaseBusiness<ID extends Serializable, EN, EN_VO, ADD_CMD, MOD_CMD,
+        QUERY_CMD extends PageReq, DAO extends JpaRepository<EN, ID> & JpaSpecificationExecutor<EN>> {
     @Autowired
     protected DAO dao;
 
@@ -30,50 +34,50 @@ public abstract class JPABaseBusiness<ID extends Serializable, ENTITY, ENTITY_VO
         throw new IllegalArgumentException(msgStr);
     }
 
-    protected ENTITY getById(ID id) {
+    protected EN getById(ID id) {
         if (null == id) {
             throwBusinessException("传入ID为空");
         }
-        Optional<ENTITY> optional = dao.findById(id);
+        Optional<EN> optional = dao.findById(id);
         if (!optional.isPresent()) {
             throwBusinessException("传入ID错误");
         }
         return optional.get();
     }
 
-    protected ENTITY_VO add(ADD_CMD cmd) {
-        ENTITY entity = addToEntity(cmd);
+    protected EN_VO add(ADD_CMD cmd) {
+        EN entity = addToEntity(cmd);
         authExist(entity);
         entity = newObjSetId(entity);
         dao.save(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO delete(ID id) {
-        ENTITY entity = getById(id);
+    protected EN_VO delete(ID id) {
+        EN entity = getById(id);
         entity = dealDelete(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO modify(MODIFY_CMD cmd) {
-        ENTITY entity = modifyInOldEntity(cmd, getById(getModifyCmdId(cmd)));
+    protected EN_VO modify(MOD_CMD cmd) {
+        EN entity = modifyInOldEntity(cmd, getById(getModifyCmdId(cmd)));
         authExist(entity);
         dao.save(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO query(ID id) {
+    protected EN_VO query(ID id) {
         return entityToVo(Collections.singletonList(getById(id)), null).get(0);
     }
 
-    protected List<ENTITY_VO> list(QUERY_CMD cmd) {
+    protected List<EN_VO> list(QUERY_CMD cmd) {
         return entityToVo(dao.findAll(cmdToSpecification(cmd)), cmd);
     }
 
-    protected PageResp<ENTITY_VO> page(QUERY_CMD cmd) {
-        Page<ENTITY> entityPage = dao
+    protected PageResp<EN_VO> page(QUERY_CMD cmd) {
+        Page<EN> entityPage = dao
                 .findAll(cmdToSpecification(cmd), PageRequest.of(cmd.getPageNum() - 1, cmd.getPageSize()));
-        PageResp<ENTITY_VO> result = new DSUtil.PageResp<>();
+        PageResp<EN_VO> result = new DSUtil.PageResp<>();
         result.setPageNum(entityPage.getNumber() + 1);
         result.setPageSize(entityPage.getSize());
         result.setTotalNum((int) entityPage.getTotalElements());
@@ -82,23 +86,27 @@ public abstract class JPABaseBusiness<ID extends Serializable, ENTITY, ENTITY_VO
         return result;
     }
 
-    protected ENTITY newObjSetId(ENTITY entity) {
+    protected Specification<EN> cmdToSpecification(QUERY_CMD cmd) {
+        return (root, query, cb) -> cmdToPredicate(cmd, root, query, cb);
+    }
+
+    protected EN newObjSetId(EN entity) {
         return entity;
     }
 
-    protected void authExist(ENTITY entity) {
+    protected void authExist(EN entity) {
     }
 
     /*************************************************抽象方法*************************************************/
-    protected abstract ENTITY addToEntity(ADD_CMD cmd);
+    protected abstract EN addToEntity(ADD_CMD cmd);
 
-    protected abstract ENTITY modifyInOldEntity(MODIFY_CMD cmd, ENTITY oldEntity);
+    protected abstract EN modifyInOldEntity(MOD_CMD cmd, EN oldEntity);
 
-    protected abstract ID getModifyCmdId(MODIFY_CMD cmd);
+    protected abstract ID getModifyCmdId(MOD_CMD cmd);
 
-    protected abstract ENTITY dealDelete(ENTITY entity);
+    protected abstract EN dealDelete(EN entity);
 
-    protected abstract List<ENTITY_VO> entityToVo(List<ENTITY> dataList, QUERY_CMD cmd);
+    protected abstract List<EN_VO> entityToVo(List<EN> dataList, QUERY_CMD cmd);
 
-    protected abstract Specification<ENTITY> cmdToSpecification(QUERY_CMD cmd);
+    protected abstract Predicate cmdToPredicate(QUERY_CMD cmd, Root<EN> root, CriteriaQuery<?> query, CriteriaBuilder cb);
 }
--- src/main/java/project/base/MybatisPlusBaseBusiness.java
+++ src/main/java/project/base/MybatisPlusBaseBusiness.java
@@ -20,8 +20,8 @@ import static project.base.DSUtil.PageResp;
  * @Author : 慕君Dxl
  * @CreateTime : 2024/4/25 14:54
  */
-public abstract class MybatisPlusBaseBusiness<ID extends Serializable, ENTITY,
-        ENTITY_VO, ADD_CMD, MODIFY_CMD, QUERY_CMD extends PageReq, DAO extends BaseMapper<ENTITY>> {
+public abstract class MybatisPlusBaseBusiness<ID extends Serializable, EN,
+        EN_VO, ADD_CMD, MOD_CMD, QUERY_CMD extends PageReq, DAO extends BaseMapper<EN>> {
     @Autowired
     protected DAO dao;
 
@@ -29,50 +29,50 @@ public abstract class MybatisPlusBaseBusiness<ID extends Serializable, ENTITY,
         throw new IllegalArgumentException(msgStr);
     }
 
-    protected ENTITY getById(ID id) {
+    protected EN getById(ID id) {
         if (null == id) {
             throwBusinessException("传入ID为空");
         }
-        ENTITY entity = dao.selectById(id);
+        EN entity = dao.selectById(id);
         if (null == entity) {
             throwBusinessException("传入ID错误");
         }
         return entity;
     }
 
-    protected ENTITY_VO add(ADD_CMD cmd) {
-        ENTITY entity = addToEntity(cmd);
+    protected EN_VO add(ADD_CMD cmd) {
+        EN entity = addToEntity(cmd);
         authExist(entity);
         entity = newObjSetId(entity);
         dao.insert(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO delete(ID id) {
-        ENTITY entity = getById(id);
+    protected EN_VO delete(ID id) {
+        EN entity = getById(id);
         entity = dealDelete(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO modify(MODIFY_CMD cmd) {
-        ENTITY entity = modifyInOldEntity(cmd, getById(getModifyCmdId(cmd)));
+    protected EN_VO modify(MOD_CMD cmd) {
+        EN entity = modifyInOldEntity(cmd, getById(getModifyCmdId(cmd)));
         authExist(entity);
         dao.updateById(entity);
         return entityToVo(Collections.singletonList(entity), null).get(0);
     }
 
-    protected ENTITY_VO query(ID id) {
+    protected EN_VO query(ID id) {
         return entityToVo(Collections.singletonList(getById(id)), null).get(0);
     }
 
-    protected List<ENTITY_VO> list(QUERY_CMD cmd) {
+    protected List<EN_VO> list(QUERY_CMD cmd) {
         return entityToVo(dao.selectList(cmdToWrapper(cmd)), cmd);
     }
 
-    protected PageResp<ENTITY_VO> page(QUERY_CMD cmd) {
-        IPage<ENTITY> entityPage = dao
+    protected PageResp<EN_VO> page(QUERY_CMD cmd) {
+        IPage<EN> entityPage = dao
                 .selectPage(new Page<>(cmd.getPageNum(), cmd.getPageSize()), cmdToWrapper(cmd));
-        PageResp<ENTITY_VO> result = new DSUtil.PageResp<>();
+        PageResp<EN_VO> result = new DSUtil.PageResp<>();
         result.setPageNum((int) entityPage.getCurrent());
         result.setPageSize((int) entityPage.getSize());
         result.setTotalNum((int) entityPage.getTotal());
@@ -81,25 +81,25 @@ public abstract class MybatisPlusBaseBusiness<ID extends Serializable, ENTITY,
         return result;
     }
 
-    protected LambdaQueryWrapper<ENTITY> cmdToWrapper(QUERY_CMD cmd) {
-        return cmdInWrapper(Wrappers.<ENTITY>lambdaQuery(), cmd);
+    protected LambdaQueryWrapper<EN> cmdToWrapper(QUERY_CMD cmd) {
+        return cmdInWrapper(Wrappers.<EN>lambdaQuery(), cmd);
     }
 
-    protected void authExist(ENTITY entity) {
+    protected void authExist(EN entity) {
     }
 
     /*************************************************抽象方法*************************************************/
-    protected abstract ENTITY newObjSetId(ENTITY entity);
+    protected abstract EN newObjSetId(EN entity);
 
-    protected abstract ENTITY addToEntity(ADD_CMD cmd);
+    protected abstract EN addToEntity(ADD_CMD cmd);
 
-    protected abstract ENTITY modifyInOldEntity(MODIFY_CMD cmd, ENTITY oldEntity);
+    protected abstract EN modifyInOldEntity(MOD_CMD cmd, EN oldEntity);
 
-    protected abstract ID getModifyCmdId(MODIFY_CMD cmd);
+    protected abstract ID getModifyCmdId(MOD_CMD cmd);
 
-    protected abstract ENTITY dealDelete(ENTITY entity);
+    protected abstract EN dealDelete(EN entity);
 
-    protected abstract List<ENTITY_VO> entityToVo(List<ENTITY> dataList, QUERY_CMD cmd);
+    protected abstract List<EN_VO> entityToVo(List<EN> dataList, QUERY_CMD cmd);
 
-    protected abstract LambdaQueryWrapper<ENTITY> cmdInWrapper(LambdaQueryWrapper<ENTITY> wrapper, QUERY_CMD cmd);
+    protected abstract LambdaQueryWrapper<EN> cmdInWrapper(LambdaQueryWrapper<EN> wrapper, QUERY_CMD cmd);
 }
