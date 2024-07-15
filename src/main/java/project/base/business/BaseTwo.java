3e630b88385a734153fcf290bfaeff3e594bec41
daixiaolong <982424518@qq.com>
2024-07-15 17:19:56 +0800

    添加测试

--- src/main/java/project/base/business/BaseTwo.java
+++ src/main/java/project/base/business/BaseTwo.java
@@ -23,7 +23,6 @@ public abstract class BaseTwo<ID extends Serializable, EN extends InterfaceOfDel
 
     protected void dealDelete(EN obj) {
         obj.deleteDealMark();
-
         //JPA
         dao.save(obj);
         //MYBATIS-PLUS
