f9a38a98efba0f30f57f5c12498127f685882860
daixiaolong <982424518@qq.com>
2024-07-16 13:56:53 +0800

    添加测试

--- src/main/java/project/base/demopg/controller/DemoController.java
+++ src/main/java/project/base/demopg/controller/DemoController.java
@@ -85,7 +85,9 @@ public class DemoController {
     @Operation(summary = "数据导出")
     public void dataExport(@RequestParam(required = false) String name,
                            HttpServletRequest request, HttpServletResponse response) {
-        demoBusiness.dataExport(new DemoListCmd().name(name), request, response);
+        DemoListCmd cmd = new DemoListCmd();
+        cmd.name(name);
+        demoBusiness.dataExport(cmd, request, response);
     }
 
     @PostMapping("/dataImport")
