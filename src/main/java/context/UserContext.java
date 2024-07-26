71c59c57d27438ce45bcc2d79e7f549cd431c80e
daixiaolong <982424518@qq.com>
2024-07-26 09:39:18 +0800

    添加测试

--- src/main/java/context/UserContext.java
+++ src/main/java/context/UserContext.java
@@ -8,26 +8,20 @@ import java.util.Map;
 
 @Data
 public class UserContext implements Serializable {
-    private Long id;
-    private String name;
-    private String account;
+    private String userId;
+    private String userName;
     private boolean superAdmin;
     private List<String> roleCodes;
     private List<String> permissions;
     private Map<String, String> extMap;
 
-    public UserContext id(Long id) {
-        this.id = id;
+    public UserContext id(String userId) {
+        this.userId = userId;
         return this;
     }
 
-    public UserContext name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public UserContext account(String account) {
-        this.account = account;
+    public UserContext name(String userName) {
+        this.userName = userName;
         return this;
     }
 
