3a91710e26bc89e48ac990968299dd7979c5cc21
daixiaolong <982424518@qq.com>
2024-07-29 13:55:03 +0800

    添加测试

--- src/main/java/basejpa/business/BaseQLP.java
+++ src/main/java/basejpa/business/BaseQLP.java
@@ -3,24 +3,17 @@ package basejpa.business;
 import basejpa.dao.BaseDao;
 import basejpa.handle.DataTool;
 import com.zx.core.base.api.Page;
-import com.zx.core.base.exception.BizException;
 import com.zx.core.base.form.Query;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-
-import static basejpa.util.DSUtil.trueThrow;
 
 /**
  * Query-List-Page
@@ -28,28 +21,8 @@ import static basejpa.util.DSUtil.trueThrow;
  * @Author : 慕君Dxl
  * @CreateTime : 2024/7/19 14:43
  */
-public abstract class BaseQLP<ID, EN, EN_VO, QUERY_CMD extends Query, DAO extends BaseDao<EN, ID>> {
-    @Autowired
-    protected DataSource dataSource;
-    @Autowired
-    protected EntityManager entityManager;
-    @Autowired
-    protected DAO dao;
-
-    protected void throwBizEx(String msg) {
-        throw bizEx(msg);
-    }
-
-    protected BizException bizEx(String msgStr) {
-        return new BizException(msgStr);
-    }
-
-    protected EN getById(ID id) {
-        trueThrow(null == id, bizEx("传入ID为空"));
-        Optional<EN> optional = dao.findById(id);
-        trueThrow(!optional.isPresent(), bizEx("传入ID错误"));
-        return optional.get();
-    }
+public abstract class BaseQLP<ID, EN, EN_VO, QUERY_CMD extends Query, DAO extends BaseDao<EN, ID>>
+        extends BaseTop<EN, ID, DAO> {
 
     public EN_VO query(ID id) {
         return entityToVo(Collections.singletonList(getById(id)), null).get(0);
